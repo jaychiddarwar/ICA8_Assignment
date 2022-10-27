06e2256846d1b2c0c0f1e5a58a269de45e69499a
Jay Chiddarwar <jchiddar@asu.edu>
2022-10-27 11:39:41 -0700

    test case 1 completed and initial logic written

---
+++ ICA8_testing/src/main/java/org/example/urinalTest.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class urinalTest {
+    @Test
+    public void sample(){
+        assertEquals(1, urinals.logic("10001"));
+        System.out.println("=== Jay Chiddarwar == TEST TWO EXECUTED ====");
+    }
+}
---
+++ ICA8_testing/src/main/java/org/example/urinals.java
@@ -0,0 +1,41 @@
+package org.example;
+
+public class urinals {
+    public static void main(String[] args) {
+        String str = "01000";
+        int ans = logic(str);
+
+
+
+
+    }
+
+    public void openFile(){
+
+    }
+
+    public static int logic(String inp){
+        char[] temp = inp.toCharArray();
+        int counter = 0;
+        if (temp[0] == '0' && temp[1] == '0' ){
+            counter++;
+            temp[0] = '1';
+        }
+        if (temp[inp.length() -1 ] == '0' && temp[inp.length() -2] == '0'){
+            counter ++;
+            temp[inp.length() -1 ] = '1';
+        }
+
+        for (int i =1 ; i < temp.length -1 ; i++){
+            if (i -1 > 0 && i +1 < temp.length && temp[i-1] == '0' && temp[i+1] == '0'){
+                counter++;
+                temp[i] = '1';
+            }
+
+        }
+
+//        System.out.println(temp);
+//        System.out.println(counter);
+        return counter;
+    }
+}
